3575db532c666140b09d02cdd7f3b9b371b9e959
Evgeniya Vorontsova <21277787+evorontsova@users.noreply.github.com>
2021-09-19 22:14:55 +0200

    Update Pascal_triangle.py

--- 118/Pascal_triangle.py
+++ 118/Pascal_triangle.py
@@ -8,7 +8,7 @@ LC Problem 118 Pascal's triangle
 Given an integer numRows, return the first numRows of Pascal's triangle.
 
 In Pascal's triangle, each number is the sum of the two numbers 
-directly above it as shown:
+directly above it
 
 Example 1:
 Input: numRows = 5
