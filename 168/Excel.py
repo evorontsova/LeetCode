1595421fa5667524cec547086217ff377a5b177c
Evgeniya Vorontsova <21277787+evorontsova@users.noreply.github.com>
2021-09-26 20:38:08 +0200

    Update Excel.py

--- 168/Excel.py
+++ 168/Excel.py
@@ -9,7 +9,6 @@ LC Problem 168  Excel Sheet Column Title
 Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
 
 For example:
-
 A -> 1
 B -> 2
 C -> 3
@@ -20,22 +19,18 @@ AB -> 28
 ...
 
 Example 1:
-
 Input: columnNumber = 1
 Output: "A"
 
 Example 2:
-
 Input: columnNumber = 28
 Output: "AB"
 
 Example 3:
-
 Input: columnNumber = 701
 Output: "ZY"
 
 Example 4:
-
 Input: columnNumber = 2147483647
 Output: "FXSHRXW"
 
@@ -73,8 +68,6 @@ class Solution:
         for i in l[::-1]:
             rez = rez + i
         return rez
-
-        
 
 class_instance = Solution()
 rez = Solution.convertToTitle(class_instance, 51)
